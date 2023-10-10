fbf57e16099c113182770b83cc28c21d66b1d5ab
1Zod <nathanmarrega@gmail.com>
2023-10-10 19:07:19 -0300

    estruturando_banco_prisma

--- dist/app.controller.js
+++ dist/app.controller.js
@@ -12,12 +12,27 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppController = void 0;
 const common_1 = require("@nestjs/common");
 const app_service_1 = require("./app.service");
+const prisma_service_1 = require("./database/prisma.service");
 let AppController = class AppController {
-    constructor(appService) {
+    constructor(appService, prisma) {
         this.appService = appService;
+        this.prisma = prisma;
     }
     teste() {
         return this.appService.getHello();
+    }
+    async teste2() {
+        const dataFormatoOriginal = new Date('10-10-2023');
+        const dataFormatoISO = dataFormatoOriginal.toISOString();
+        const member = await this.prisma.calendario.create({
+            data: {
+                id: 123,
+                data: dataFormatoISO,
+            },
+        });
+        return {
+            member,
+        };
     }
 };
 exports.AppController = AppController;
@@ -27,8 +42,15 @@ __decorate([
     __metadata("design:paramtypes", []),
     __metadata("design:returntype", String)
 ], AppController.prototype, "teste", null);
+__decorate([
+    (0, common_1.Get)('bbb'),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", []),
+    __metadata("design:returntype", Promise)
+], AppController.prototype, "teste2", null);
 exports.AppController = AppController = __decorate([
     (0, common_1.Controller)(),
-    __metadata("design:paramtypes", [app_service_1.AppService])
+    __metadata("design:paramtypes", [app_service_1.AppService,
+        prisma_service_1.PrismaService])
 ], AppController);
 //# sourceMappingURL=app.controller.js.map
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,33 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaService } from './database/prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private prisma: PrismaService,
+  ) {}
 
   @Get('aaa')
   teste(): string {
     return this.appService.getHello();
+  }
+
+  @Get('bbb')
+  async teste2() {
+    const dataFormatoOriginal = new Date('10-10-2023');
+    const dataFormatoISO = dataFormatoOriginal.toISOString();
+
+    const member = await this.prisma.calendario.create({
+      data: {
+        id: 123,
+        data: dataFormatoISO,
+      },
+    });
+
+    return {
+      member,
+    };
   }
 }
